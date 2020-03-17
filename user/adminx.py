6ee76fd3bcc55a3a98da8763db7b0de3159e9bab
曾衍仁 <zengyanren@gdmcmc.cn>
2020-03-17 18:49:00 +0800

    增加useradmin

--- user/adminx.py
+++ user/adminx.py
@@ -1,30 +1,60 @@
 import xadmin
 from .models import Role, UserProfile, Department
+from django.contrib.auth.forms import (UserCreationForm, UserChangeForm,
+                                       AdminPasswordChangeForm, PasswordChangeForm)
+from xadmin.layout import Fieldset, Main, Side, Row, FormHelper
 
 
 class UserProfilesAdmin(object):
-    list_display = ['id', 'username', 'mobile', 'email', 'date_joined', 'modifyCode']
-    readonly_fields = ['last_login', 'date_joined']
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'mobile')
-    # style_fields = {'user_permissions': 'm2m_transfer', 'groups': 'm2m_transfer'}
+    change_user_password_template = None
+    list_display = ('username', 'dept', 'role')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+    style_fields = {'user_permissions': 'm2m_transfer'}
+    model_icon = 'fa fa-user'
+    relfield_style = 'fk-ajax'
     exclude = ('user_permissions', 'groups')
-    # 多对多样式字段支持过滤
-    filter_horizontal = ('role',)
-    # 多对多穿梭框样式
-    style_fields = {'role': 'm2m_transfer'}
 
-    def modifyCode(self, obj):
-        return """<a href="/xadmin/user/userprofile/%d/password/" target="_parent">%s</a>""" % (obj.id, '修改密码')
-
-    modifyCode.short_description = "修改密码"
-    modifyCode.allow_tags = True
-    modifyCode.is_column = True
+    # def get_field_attrs(self, db_field, **kwargs):
+    #     attrs = super(UserProfilesAdmin, self).get_field_attrs(db_field, **kwargs)
+    #     if db_field.name == 'user_permissions':
+    #         attrs['form_class'] = PermissionModelMultipleChoiceField
+    #     return attrs
 
     def get_model_form(self, **kwargs):
         if self.org_obj is None:
-            self.fields = ['username', 'mobile', 'is_staff']
+            self.form = UserCreationForm
+        else:
+            self.form = UserChangeForm
+        return super(UserProfilesAdmin, self).get_model_form(**kwargs)
 
-        return super().get_model_form(**kwargs)
+    def get_form_layout(self):
+        if self.org_obj:
+            self.form_layout = (
+                Main(
+                    Fieldset('',
+                             'username', 'password',
+                             css_class='unsort no_title'
+                             ),
+                    Fieldset('Personal info',
+                             Row('first_name', 'last_name'),
+                             'email'
+                             ),
+                    # Fieldset('Permissions',
+                    #          'groups', 'user_permissions'
+                    #          ),
+                    Fieldset('Important dates',
+                             'last_login', 'date_joined'
+                             ),
+                ),
+                Side(
+                    Fieldset('Status',
+                             'is_active', 'is_staff', 'is_superuser',
+                             ),
+                )
+            )
+        return super(UserProfilesAdmin, self).get_form_layout()
 
 
 class RoleAdmin(object):
