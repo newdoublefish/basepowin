c914b70154108ec54c0316e709a3f9ab9a7e61d5
曾衍仁 <zengyanren@gdmcmc.cn>
2020-03-17 18:20:32 +0800

    增加useradmin 修改密码样式

--- user/adminx.py
+++ user/adminx.py
@@ -3,7 +3,28 @@ from .models import Role, UserProfile, Department
 
 
 class UserProfilesAdmin(object):
-    list_display = ('id', 'username', )
+    list_display = ['id', 'username', 'mobile', 'email', 'date_joined', 'modifyCode']
+    readonly_fields = ['last_login', 'date_joined']
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'mobile')
+    # style_fields = {'user_permissions': 'm2m_transfer', 'groups': 'm2m_transfer'}
+    exclude = ('user_permissions', 'groups')
+    # 多对多样式字段支持过滤
+    filter_horizontal = ('role',)
+    # 多对多穿梭框样式
+    style_fields = {'role': 'm2m_transfer'}
+
+    def modifyCode(self, obj):
+        return """<a href="/xadmin/user/userprofile/%d/password/" target="_parent">%s</a>""" % (obj.id, '修改密码')
+
+    modifyCode.short_description = "修改密码"
+    modifyCode.allow_tags = True
+    modifyCode.is_column = True
+
+    def get_model_form(self, **kwargs):
+        if self.org_obj is None:
+            self.fields = ['username', 'mobile', 'is_staff']
+
+        return super().get_model_form(**kwargs)
 
 
 class RoleAdmin(object):
@@ -22,5 +43,6 @@ class DepartmentAdmin(object):
 
 xadmin.site.register(Role, RoleAdmin)
 xadmin.site.register(Department, DepartmentAdmin)
-# xadmin.site.unregister(UserProfile)
-# xadmin.site.register(UserProfile, UserProfilesAdmin)
+xadmin.site.unregister(UserProfile)
+xadmin.site.register(UserProfile, UserProfilesAdmin)
+
