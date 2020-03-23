5f1616e9659a8ae87fe76386ca87b16f92483323
曾衍仁 <zengyanren@gdmcmc.cn>
2020-03-23 19:05:20 +0800

    增加create方法

--- procedure/views.py
+++ procedure/views.py
@@ -9,6 +9,7 @@ from django_filters.rest_framework import FilterSet
 import django_filters
 from .serializers import MopSerializer, ProcedureSerializer, ReceiptSerializer, TaskSerializer
 from .models import Mop, Procedure, Receipt, Task
+from django.db import transaction
 
 
 class MopViewSet(GenericViewSet,
@@ -20,6 +21,22 @@ class MopViewSet(GenericViewSet,
     queryset = Mop.objects.all()
     serializer_class = MopSerializer
     filterset_fields = ('manufacture_order_name',)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # TODO:判断有没有重名的
+        with transaction.atomic():
+            save_id = transaction.savepoint()
+            try:
+                instance = serializer.save()
+                print(instance.id)
+            except Exception as e:
+                transaction.savepoint_rollback(save_id)
+                return Response({"status": "error", "msg": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            transaction.savepoint_commit(save_id)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class ProcedureViewSet(GenericViewSet,
