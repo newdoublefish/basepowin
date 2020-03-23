ef98cde20becdb63480dad9b89fcc4f34ab2fd3f
曾衍仁 <zengyanren@gdmcmc.cn>
2020-03-23 16:21:11 +0800

    为流程相关信息栏添加图标

--- procedure/adminx.py
+++ procedure/adminx.py
@@ -1,7 +1,25 @@
 import xadmin
 from .models import Mop, Procedure, Receipt, Task
 
-xadmin.site.register(Mop)
-xadmin.site.register(Procedure)
-xadmin.site.register(Receipt)
-xadmin.site.register(Task)
+
+class MopAdmin(object):
+    model_icon = 'fa fa-bars '
+    list_display = ('manufacture_order_name', 'sell_order_name', 'part_no_name', 'quantity', 'status')
+
+
+class ProcedureAdmin(object):
+    model_icon = "fa fa-hand-o-right"
+
+
+class ReceiptAdmin(object):
+    model_icon = "fa fa-paperclip"
+
+
+class TaskAdmin(object):
+    model_icon = "fa fa-tasks"
+
+
+xadmin.site.register(Mop, MopAdmin)
+xadmin.site.register(Procedure, ProcedureAdmin)
+xadmin.site.register(Receipt, ReceiptAdmin)
+xadmin.site.register(Task, TaskAdmin)
