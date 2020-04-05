7edc8ccfcc5b66c747bf058a5e5cfe4cbbf892a6
曾衍仁 <zengyanren@foxmail.com>
2020-04-05 16:34:11 +0800

    增加显示ID

--- version/adminx.py
+++ version/adminx.py
@@ -3,7 +3,7 @@ from .models import Version
 
 
 class VersionAdmin(object):
-    list_display = ('terminal_type', 'version', 'is_valid', 'app_url')
+    list_display = ('id', 'terminal_type', 'version', 'is_valid', 'app_url')
     model_icon = 'fa fa-bookmark'
 
 
